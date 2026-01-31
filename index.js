5fc0e13e57e7dc712e009c557489d0bb6ea96e3c
vish9043 <vish9043@gmail.com>
2026-01-31 17:05:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ window.initMap = initMap;
 
 $(window).on('load', function () {
   // sheetID you can find in the URL of your spreadsheet after "spreadsheet/d/"
-  const sheetId = "1B3qVHKjD27Q33FpOtBHN57aUJBAMS5FAfYzXUAZWTUY";
+  const sheetId = "1hnat5djjbsckCo9kRU0GDBILBnii2b-Cc_M6ju80VZc";
   // sheetName is the name of the TAB in your spreadsheet (default is "Sheet1")
   const sheetName = encodeURIComponent("Sheet1");
   const sheetURL = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?tqx=out:csv&sheet=${sheetName}`;
